89423a5a56f14ddfc231c2ab5d0bc519cbb671ea
Vivek Ranjan Sahoo <vivekranjansahoo81@gmail.com>
2022-12-11 11:36:05 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+//console.log("hellow world")
+
+//Day-1
+
+//-----------------------------------------------------
+/*
+const a=10;
+const b=20;
+c=a+b;
+console.log(c);
+
+*/
+
+//Day-2
+
+//---------------------------------------------------------------
+
+
+var vivek={name:"vivek ranjan sahoo",age:20,email:"Vivekranjan@gmail.com",};
+console.log(vivek.name);
