22f8450597b90db832e7eeb9caff2f0a9b808c24
Vivek Ranjan Sahoo <vivekranjansahoo81@gmail.com>
2022-12-12 20:10:09 +0530

    arrow function

--- main.js
+++ main.js
@@ -14,9 +14,44 @@ console.log(c);
 //Day-2
 
 //---------------------------------------------------------------
-
+/*
 
 var vivek={name:"vivek ranjan sahoo",age:20,email:"Vivekranjan@gmail.com",};
 console.log(vivek.name);
 
+
+*/
+
 //Day -3
+//--------------------------------------------
+
+// 1-parameter , non parameter function
+/*
+var person=function(a,b){
+    var c=a+b;
+    console.log("the addition of two number"+c)
+}
+
+
+
+person(2,3);
+
+ arrow fuction
+  
+var users=function(){
+    console.log("this is arrow fuction");
+}
+
+
+convet arrow fuction
+
+var users=()=>console.log("this is arrow fuction");
+
+var users=(a,b)=>{
+    var c=a+b;
+    console.log("the addition of two number"+c);
+}
+
+users(2,4);
+
+*/
